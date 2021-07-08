6faf59b5cfc47b7c05fa51a0eab551f4667baf9a
FernDEVIL <80749887+FernDEVIL@users.noreply.github.com>
2021-07-08 15:44:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("Hi, I am console screen");
+var f_name = "Ashneel "
+var l_name = "Routray"
+var full_name = f_name.concat(l_name);
+console.log(full_name);
+var m1 = "3";
+var m2 = "5";
+var multiply = m1 * m2;
+console.log(multiply);
+var s1 = "11";
+var s2 = "6";
+var subtract = s1 - s2;
+console.log(subtract);
+var a1 = "15";
+var a2 = "35";
+var add = a1 + a2;
+console.log(add);
+var d1 = "10";
+var d2 = "5";
+var divide = d1 / d2;
+console.log(divide);
